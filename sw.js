87b6cad887295f746f185fd5635b302c4515fcf3
BairanOwO <105583227+BairanOwO@users.noreply.github.com>
2025-05-19 23:41:37 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('pwa-cache-v1').then(cache => {
-      return cache.addAll(['./', './index.html', './style.css', './script.js', './manifest.json']);
-    })
+    caches.open('pwa-cache-v2').then(cache => cache.addAll([
+      './', './index.html', './style.css', './script.js', './manifest.json', 'assets/bg-music.mp3', 'assets/photos/1.jpg', 'assets/photos/2.jpg', 'assets/photos/3.jpg'
+    ]))
   );
 });
 self.addEventListener('fetch', event => {
